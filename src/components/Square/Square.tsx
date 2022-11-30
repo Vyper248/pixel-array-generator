a3d56c3c916aa4ada7c3c86f2d424591ae11f3b1
Christopher T Wilson <vyper374@hotmail.com>
2022-11-30 15:23:49 +0000

    Fixed bug with clearning not resetting square props

--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -1,15 +1,19 @@
-import { createSignal } from 'solid-js';
+import { createEffect, createSignal } from 'solid-js';
 import StyledSquare from './Square.style';
 
 export type SquareProps = {
     x: number;
     y: number;
-    active?: boolean;
+    active: boolean;
     onClick: (x:number, y:number, active:boolean) => void;
 }
 
 const Square = (props: SquareProps) => {
     const [active, setActive] = createSignal(false);
+
+    createEffect(() => {
+        if (props.active !== active()) setActive(props.active);
+    });
 
     const onClick = () => {
         props.onClick(props.x, props.y, !active());
